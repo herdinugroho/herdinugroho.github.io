40c52065523e3830a365db3fae525db9168cfdb2
herdinugroho <herdi.nugroho1912@gmail.com>
2021-04-21 19:58:33 +0700

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,31 @@
+
+  
+// Scrol otomatis saat link di klik
+$('.page-scroll').on('click', function(e){
+
+  var tujuan = $(this).attr('href');
+
+  var elemenTujuan = $(tujuan);
+  
+  $('body').animate({
+    scrollTop: elemenTujuan.offset(). top - 100
+  }, 1250, 'swing');
+  
+  e.preventDefault();
+
+
+});
+
+
+function aos_init() {
+    AOS.init({
+      duration: 1000,
+      easing: "ease-in-out",
+      once: true,
+      mirror: false
+    });
+  }
+  window.addEventListener('load', () => {
+    aos_init();
+  });
+
